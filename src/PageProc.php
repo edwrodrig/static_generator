547834ba03b0000101ae690b86be3721a37af897
Edwin Rodríguez <edwrodrig@gmail.com>
2017-12-26 12:26:43 -0300

    Changes

--- src/PageProc.php
+++ src/PageProc.php
@@ -3,21 +3,6 @@ namespace edwrodrig\static_generator;
 
 class PageProc extends Page {
 
-public function output() {
-  $output = Site::get()->output($this->input_relative_path);
-  dirname($output);
-}
-
-public function prepare_output() {
-  if ( is_null($this->output_absolute_path) ) {
-    $output = Site::get()->output($this->input_relative_path);
-    $output = dirname($output, 0777, true);
-    $this->output_absolute_path = $output;
-  }
-  
-  return parent::prepare_output();
-}
-
 public function generate() {
   $this->log(sprintf("Proccessing file [%s]...\n", $this->input_relative_path));
 
