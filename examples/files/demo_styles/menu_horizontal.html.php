f99cf27669444dcca203f4904b6d1cfde0b393c2
Edwin Rodríguez <edwrodrig@gmail.com>
2018-05-29 16:13:11 -0400

    Changes

---
+++ examples/files/demo_styles/menu_horizontal.html.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="<?=$this->url('/style.css')?>">
+</head>
+<body>
+<div style="display:flex;">
+    <div class="popup-menu-horizontal">
+        <div style="width:100px;background-color: red">A</div>
+        <menu><div>
+                <div>Menu 1</div>
+                <div>Menu 2</div>
+            </div></menu>
+    </div>
+    <div class="popup-menu-horizontal">
+        <div style="width:100px;background-color: blue">B</div>
+        <menu><div>
+                <div>Menu 4</div>
+                <div>Menu 5</div>
+            </div></menu>
+    </div>
+</div>
+</body>
+</html>
---
+++ examples/files/demo_styles/menu_vertical.html.php
@@ -0,0 +1,27 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="<?=$this->url('/style.css')?>">
+</head>
+<body>
+<div style="width:100px;">
+    <div class="popup-menu-vertical">
+        <div style="width:100%;background-color: red">A</div>
+        <menu><div>
+                <div>Menu 1</div>
+                <div>Menu 2</div>
+            </div></menu>
+    </div>
+    <div class="popup-menu-vertical">
+        <div style="width:100%;background-color: blue">B</div>
+        <menu><div>
+                <div>Menu 4</div>
+                <div>Menu 5</div>
+            </div></menu>
+    </div>
+</div>
+</body>
+</html>
