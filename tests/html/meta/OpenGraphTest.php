1bec29ed5536a5d633ccc577d6714a9742916b40
Edwin Rodríguez <edwrodrig@gmail.com>
2018-05-30 22:24:42 -0400

    Opengraph datetime test

--- tests/html/meta/OpenGraphTest.php
+++ tests/html/meta/OpenGraphTest.php
@@ -8,6 +8,7 @@
 
 namespace test\edwrodrig\static_generator\html\meta;
 
+use DateTime;
 use edwrodrig\static_generator\html\meta\OpenGraph;
 use PHPUnit\Framework\TestCase;
 
@@ -26,5 +27,20 @@ class OpenGraphTest extends TestCase
         $output = ob_get_clean();
 
         $this->assertEquals('<meta property="og:type" content="website"/><meta property="og:description" content="hola"/>', $output);
+    }
+
+    public function testDate()
+    {
+        $s = new OpenGraph;
+        $s->setType('website');
+        $s->setDescription('hola');
+        $s->setUpdateTime(new DateTime('2018-01-01'));
+
+        ob_start();
+        $s->print();
+
+        $output = ob_get_clean();
+
+        $this->assertEquals('<meta property="og:type" content="website"/><meta property="og:description" content="hola"/><meta property="og:update_time" content="2018-01-01T00:00:00+0000"/>', $output);
     }
 }
